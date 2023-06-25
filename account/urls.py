f3bee6fc032665ea1d70a18b4dc22737efba3ffd
Phibeon <phibeon@yahoo.com>
2023-06-25 08:22:12 +0200

    added passwors rest password change used he default django.contrib.auth.urls

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,22 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .import views
 
-app_name = 'accounts'
-
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # password change urls
+    #path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    #path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    # password login and logout
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    # password reset
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+# own authentication
 def user_login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -21,3 +23,8 @@ def user_login(request):
     else:
         form = LoginForm()
         return render(request, 'account/login.html', {'form':form})
+    
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section' : 'dashboard'}) 
